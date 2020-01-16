8f9d93c9b4371b70e03390354a0940871ed6ef92
sharonNissanov <58398243+sharonNissanov@users.noreply.github.com>
2020-01-16 16:55:01 +0200

    running message window
    implementing the message window, while typing the wanted text,  it will show up and will scroll to the left .

---
+++ src/component/App.js
@@ -0,0 +1,16 @@
+
+import React from "react"
+import MessageWindow from '../messageSystem/MessageWindow'
+
+
+
+function App() {
+    return (
+      <div >
+        <MessageWindow  />
+   
+     </div>
+    );
+  }
+
+  export default App;
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,62 +1,10 @@
-import {CONSTANTS} from "../Actions";
-let listIDNew=3;
-const initialState = {
-    title: "This is the title",
-    OperationList: [
-       {
-           listID:0,
-           title: 'First List',
-           cards:[
-               {id:0},
-               {id:1}
-           ]
-       },
-       {
-        listID:1,
-        title: 'Second List',
-        cards:[
-            {id:0},
-            {id:1}
-        ]
-        }
-    ]
+
+const initSate = {
+    value : "",
 }
+const rootReducer = (state=initSate ,action )=>{
 
-const rootReducer = (state = initialState, action) =>{
-    let listID=3;
-    switch(action.type){
-    case CONSTANTS.ADD_LIST:
-        const newList = {
-            listID : listIDNew, 
-            title:"List num"+listIDNew,
-            cards: [] , 
-        }
-        listIDNew++; 
-    return {...state,OperationList: [...state.OperationList, newList]};
-    
-    case CONSTANTS.ADD_CARD:{
-        const newCard = {
-            id:4
-        } 
-       
-       const newOperationList= state.OperationList.map (list => {
-            if(list.listID === action.payload.listID){
-                return{
-                    ...list,
-                    cards:[...list.cards, newCard]
-                }
-            }else{
-                return list;
-            }
-        });   
-
-        return {...state, OperationList: newOperationList};
-        }
-
-
-    }
-
-    return state;
+return state;
 }
 
 export default rootReducer;
