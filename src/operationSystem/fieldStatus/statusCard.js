595728dcd3050608b5b34ae1aca23be3eedc628e
danapesah <pe.dana@gmail.com>
2020-02-23 19:25:13 +0200

    Add field status Button- doesnt function yet

--- src/operationSystem/fieldStatus/statusCard.js
+++ src/operationSystem/fieldStatus/statusCard.js
@@ -1,10 +1,41 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button';
+import Popup from "reactjs-popup";
 
 const StatusCard = ({title ,buttons, changeColor,cardID, listID, deleteCard}) =>
 {
-    console.log(listID);
-    const ifEditAble =(cardID,buttonID,listID)=>
+    let state= {
+        buttonName:""
+    }
+
+    const handleChange =(event)=>
+    {
+       if(event.target.name = "button")
+        state.buttonName= event.target.value;
+
+   }   
+
+    const addAble =()=>
+    {
+        //CHECK IF EDITABLE
+        if(true)
+            return(
+            <Popup
+            trigger={ <Button style={{width:"auto",height:"auto", marginRight:"10px"}}>+</Button>}
+            modal
+            closeOnDocumentClick>
+           <form style={{float:"center"}} >
+                <label style={{float:"center"}} >
+                    Button Title:
+                    <input  type="text" name="button" onChange={handleChange} />
+                </label>
+                 <input type="submit" value="Submit" /> 
+            </form>  
+          </Popup>)
+
+    }
+
+    const deleteAble =(cardID,buttonID,listID)=>
     {
         //CHECK IF EDITABLE
         if(true)
@@ -15,9 +46,10 @@ const StatusCard = ({title ,buttons, changeColor,cardID, listID, deleteCard}) =>
            
         <div>
         {buttons.map(button=>(<Button style={{backgroundColor: "green", width:"auto"}} onClick={changeColor}>
-        {ifEditAble(cardID,button.id,listID)}
+        {deleteAble(cardID,button.id,listID)}
         {button.titleButton} </Button>))}
         </div>
+      {addAble()}
         <div>{title}</div>
         </div>
     )
