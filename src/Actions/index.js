456f8fc559e750251c290b1f9c446d6034058f3c
danapesah <pe.dana@gmail.com>
2020-02-23 12:55:14 +0200

    Update index.js

--- src/Actions/index.js
+++ src/Actions/index.js
@@ -1,15 +1,10 @@
- 
-export * from "./OperationActions"
-export * from "./listsActionsCountDown"
-export * from "./cardsActionCountDown"
-export * from './refreshActionCountDown'
+export * from "./listsActions"
+export * from "./cardsAction"
+export * from './refreshAction'
 
 export const CONSTANTS = {
-    ADD_LIST_OPERATION: "ADD_LIST_OPERATION",
-    ADD_CARD_OPERATION: "ADD_CARD_OPERATION",
-    ADD_CARD_COUNTDOWN: "ADD_CARD_COUNTDOWN",
-    ADD_LIST_COUNTDOWN: "ADD_LIST_COUNTDOWN",
-    DRAG_HAPPENED_COUNTDOWN: "DRAG_HAPPENED_COUNTDOWN",
-    REFRESH_COUNTDOWN: "REFRESH_COUNTDOWN"
+    ADD_CARD: "ADD_CARD",
+    ADD_LIST: "ADD_LIST",
+    DRAG_HAPPENED: "DRAG_HAPPENED",
+    REFRESH: "REFRESH"
 };
-
