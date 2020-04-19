45efa7118289c87e8d404d328c911a05e0e5caad
sharonNissanov <58398243+sharonNissanov@users.noreply.github.com>
2020-04-19 23:21:25 +0300

    upload img to air-log
    upload img to air-log

--- src/operationSystem/countDownWindow/Logs.js
+++ src/operationSystem/countDownWindow/Logs.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
-import {connect } from 'react-redux'
 import TestScheduler from '../countDownWindow/TestScheduler'
+import UploadAirImg from '../countDownWindow/UploadAirImg'
 import Tabs from 'react-bootstrap/Tabs'
 import Tab from 'react-bootstrap/Tab'
 import Nav from 'react-bootstrap/Nav'
@@ -36,35 +36,11 @@ class Logs extends Component {
 <div style={{position:"absolute", top:"50px"}}>
 {this.state._chosen_tab==="#evets" ? "events" :
 this.state._chosen_tab==="#sea" ? "sea"  :
-this.state._chosen_tab==="#air" ? "air"  :
+this.state._chosen_tab==="#air" ? <div style={{backgroundColor: "#66c2ff",position:"absolute", top:"50px", left:"20px"}}><UploadAirImg/> </div> :
 <TestScheduler/>
 }</div>
 
   </div>
-    // <ul class="nav nav-tabs">
-    // <li class="nav-item">
-    // <a class="nav-link active" href="#countdown">לשונית ספירה יורדת</a>
-    // <div style={{position:"absolute", top:"50px"}} ><TestScheduler /></div>
-    // </li>
-    // <li class="nav-item">
-    // <a class="nav-link " href="#evets">לשונית לוג אירועים </a>
-    // <div style={{position:"absolute", top:"50px"}} ></div>
-    // </li>
-    // <li class="nav-item">
-    // <a class="nav-link" href="#sea">לשונית סגירות ים</a>
-    // </li>
-    // <li class="nav-item">
-    // <a class="nav-link" href="#air">לשונית סגירות אוויר</a>
-    // </li>
-    // </ul>
-    // <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
-    // <Tab eventKey="countdown" title="לשונית ספירה יורדת"
-    // onSelect={alert("kaka")}
-    // ></Tab>
-    // <Tab eventKey="evets" title=" לשונית לוג אירועים"></Tab>
-    // <Tab eventKey="sea" title="לשונית סגירות ים" ></Tab>
-    // <Tab eventKey="air" title="לשונית סגירות אוויר" ></Tab>
-    // </Tabs>
 
     )
 
@@ -72,15 +48,7 @@ this.state._chosen_tab==="#air" ? "air"  :
     }
 }
 
-
-
-
-const mapStateToProps = (state)=> ({
-    lists: state.CountDownlists.resources,
-    events: state.CountDownlists.events
-
-})
-export default connect(mapStateToProps)(Logs) ;
+export default Logs;
 
 
 
