91d4157614feb8cbd61f51b3a30d26f6b14f6765
sharonNissanov <58398243+sharonNissanov@users.noreply.github.com>
2020-06-11 12:01:29 +0300

    changes
    changes

--- src/SystemManagement/UserManagement/MyNavbar.js
+++ src/SystemManagement/UserManagement/MyNavbar.js
@@ -40,6 +40,7 @@ class MyNavbar extends Component {
 render() {
     return (
   
+<<<<<<< HEAD
       <div style={{backgroundColor:"#343a40", width : "100%" , height:"80px", color:"white",paddingLeft:"50px"}}>
       <div > 
       <h4 > CountDown System</h4> 
@@ -49,6 +50,57 @@ render() {
       {this.state._user_logged ===true ? 
       <div style={{ display:"flex"  }}>
       <Link  style={{color:"white", display:"flex",width : "60px" }} 
+=======
+  <div style={{backgroundColor:"#343a40", width : "100%" , height:"80px", color:"white",paddingLeft:"50px"}}>
+      <h4 style={{textAlign:"center"}} > CountDown System</h4> 
+      <div style={{textAlign:"center"}}>{this.state._user_logged===true ? 
+    "logged as: " +this.state._user_name+" "+this.state._user_permissions : "please login"} 
+  </div>
+  {this.state._user_logged ===true ? 
+    <div >
+     <Nav className="justify-content-end" activeKey="/home">
+     {this.state._user_permissions==="Admin" ? 
+     <div style={{ display:"flex"  }}>
+    <Nav.Item>
+      <Nav.Link 
+       onClick={()=>{this.props.dispatch(save_user_info_after_login('', '',false,''))
+       ;localStorage.clear() ; 
+         this.setState({
+         _user_name:'', 
+         _user_permissions:'',
+         _user_logged: false,
+         })
+       }}
+      href="/"  eventKey="link-3">התנתק</Nav.Link>
+    </Nav.Item>
+
+ 
+    <Nav.Item>
+      <Nav.Link href="/user"  eventKey="link-3">יצירת משתמש</Nav.Link>
+    </Nav.Item>
+
+  <Nav.Item>
+    <Nav.Link  href="/usersList" eventKey="link-2">רשימת משתמשים</Nav.Link>
+  </Nav.Item>
+  </div>
+  :null }  
+  <Nav.Item>
+    <Nav.Link href="/list" eventKey="link-1">
+      רשימת ניסויים
+    </Nav.Link>
+  </Nav.Item>
+
+   
+  </Nav>
+  </div>
+  :null }  
+  <div > 
+    
+
+  {/* {this.state._user_logged ===true ? 
+    <div style={{ display:"flex"  }}>
+    <Link  style={{color:"white", display:"flex",width : "60px" }} 
+>>>>>>> parent of 0addaa7... more changes for navbar
       onClick={()=>{this.props.dispatch(save_user_info_after_login('', '',false,''))
       ;localStorage.clear() ; 
       this.setState({
