fda13299ee27ad5ebd1ac78736d8e98fbfbccb4e
danapesah <pe.dana@gmail.com>
2020-06-17 21:50:49 +0300

    Text Scroll Fix

--- src/operationSystem/MessageWindow/TextScroller.jsx
+++ src/operationSystem/MessageWindow/TextScroller.jsx
@@ -1,13 +1,23 @@
 import React, { useState } from "react";
 import { useSpring, animated } from "react-spring";
 
-const TextScroller = ({ text }) => {
+const TextScroller = ({ text, length }) => {
   const [key, setKey] = useState(1);
-
+  let toPrecentege;
+  if(length<=150)
+    toPrecentege="100%";
+  else if(length>150 && length<=300)
+    toPrecentege="200%";
+  else if(length>300 && length<=450)
+    toPrecentege="300%";
+  else if(length>450 && length<=600)
+    toPrecentege="400%";  
+  else
+  toPrecentege="500%";  
   const scrolling = useSpring({
-    from: { transform: "translate(60%,0)" },
-    to: { transform: "translate(-60%,0)" },
-    config: { duration: 20000 },
+    from: { transform: "translate(100%,0)" },
+    to: { transform: "translate(-"+toPrecentege+",0)"},
+    config: { duration: 30000 },
     reset: true,
     //reverse: key % 2 == 0,
     onRest: () => {
